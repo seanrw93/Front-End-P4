654000a0f0c7313d3b63eedcd457f8dd21e6e2c2
seanrw93 <seanrw@gmail.com>
2023-12-10 23:23:32 +0100

    Class and html validity checks added
    Class UserData to act as mock json data for submitted form input values

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,5 +1,5 @@
-function editNav() {
-  var x = document.getElementById("myTopnav");
+function toggleResponsiveNav() {
+  const x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
   } else {
@@ -11,13 +11,79 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelector(".close")
+const form = document.forms.reserve;
 
 // launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalBtn.forEach(btn => {
+  btn.addEventListener("click", () => {
+    launchModal();
+  })
+});
+
+closeBtn.addEventListener("click", () => closeModal());
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+  disablePageScroll();
+  closeBtn.addEventListener("click", closeModal);
 }
 
+//Close modal form
+function closeModal() {  
+  modalbg.style.display = "none";
+  enablePageScroll();
 
+  closeBtn.removeEventListener("click", closeModal);
+}
+
+// Disable page scroll
+function disablePageScroll() {
+  document.body.style.overflow = "hidden";
+}
+
+// Enable page scroll
+function enablePageScroll() {
+  document.body.style.overflow = "";
+}
+
+//Class to hold form data 
+class UserData {
+  constructor(firstName, lastName, email, birthDate, tournamentsNum, location, checkbox) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.birthDate = birthDate;
+    this.tournamentsNum = tournamentsNum;
+    this.location = location;
+    this.checkbox = checkbox || [];
+  }
+
+  toJson() {
+    return {
+      firstName: this.firstName,
+      lastName: this.lastName,
+      email: this.email,
+      birthDate: this.birthDate,
+      tournamentsNum: this.tournamentsNum,
+      location: this.location,
+      checkbox: this.checkbox
+    }
+  }
+}
+
+function processFormSubmission(e) {
+  e.preventDefault();
+
+  // let userData = new UserData();
+
+  // Convert the UserData instance to JSON
+  // let jsonUserData = userData.toJson();
+  // console.log(jsonUserData);
+
+  closeModal();
+
+}
+
+form.addEventListener("submit", processFormSubmission)
