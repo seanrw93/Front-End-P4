4a77e1d1c30d1fdeabf88df6db84802d3afef08e
seanrw93 <seanrw@gmail.com>
2024-01-05 16:13:33 +0100

    Changes to HTML and JS
    HTML: required removed
    JS: Error messages placed in object to be more dynamic
    JS: Script added to remove error styles and text in closeModal(work in prog)
    JS: Script add to processFormSubmission to prevent submission if input field is empty

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -52,12 +52,30 @@ function launchModal(modal) {
 }
 
 //Close modal form
-function closeModal(modal) {  
+function closeModal(modal) {
   modal.style.display = "none";
   enablePageScroll();
+
+  // Reset custom validity for each input
+  formData.forEach(data => {
+    const input = data.querySelector("input");
+    input.setCustomValidity("");
+  });
+
+  // Remove specified attributes from each element
+  formData.forEach(data => {
+    const inputParent = data.querySelector(".formData");
+
+    // Check if inputParent is not null before accessing properties
+    if (inputParent) {
+      inputParent.style.content = "";
+      inputParent.removeAttribute("data-error");
+      inputParent.removeAttribute("data-error-visible");
+    }
+  });
+
   form.reset();
 }
-
 // Disable page scroll
 function disablePageScroll() {
   document.body.style.overflow = "hidden";
@@ -69,11 +87,11 @@ function enablePageScroll() {
 }
 
 // Get today's date and insert it as max date in date input in HTML
-function getTodaysdate() {
+function getTodaysDate() {
   const date = new Date()
   const currentYear = date.getFullYear();
-  const currentMonth = date.getMonth() + 1;
-  const currentDate = date.getDate();
+  const currentMonth = String(date.getMonth() + 1).padStart(2, '0');
+  const currentDate = String(date.getDate()).padStart(2, '0'); 
 
   return `${currentYear}-${currentMonth}-${currentDate}`
 }
@@ -132,22 +150,38 @@ function addInvalidityStyles(inputParent, input, inputError) {
   }
 }
 
+//Generate error messages to be used in check functions
+function errorMessage({minCharLength = null} = {}) {
+    const errors = {
+      firstNameError: "Please enter your first name",
+      lastNameError: "Please enter your first name",
+      birthdateError: "Please enter your birthdate",
+      minCharError: minCharLength ? `Please enter ${minCharLength} or more characters` : null
+    }
+
+    return errors
+}
+
 function handleValidation(inputParent) {
   const input = inputParent.querySelector("input");
   let inputError = null;
 
-  switch(input.type) {
-    case "text":
-      inputError = nameCheck(input);
+  switch(input.name) {
+    case "first":
+    case "last": 
+      inputError = nameCheck(input, 3);
       break;
     case "email":
       inputError = emailCheck(input);
       break;
-    case "date":
+    case "birthdate":
       inputError = ageCheck(input);
       break;
-    case "number":
+    case "quantity":
       inputError = quantityCheck(input);
+      break;
+    case "location":
+      inputError = locationCheck(input);
       break;
 
     //More cases to come for other checks
@@ -158,28 +192,24 @@ function handleValidation(inputParent) {
   addInvalidityStyles(inputParent, input, inputError);
 }
 
-function nameCheck(input) {
+function nameCheck(input, min) {
   if (input.value.trim() === "") {
     if (input.name === "first") {
-      console.log("Error: Please enter your first name");
-      return "Please enter your first name";
+      return errorMessage().firstNameError;
     } else {
-      console.log("Error: Please enter your last name");
-      return "Please enter your last name";
+      return errorMessage().lastNameError;
     }
-  } else if (input.value.length < 3) {
-    console.log("Error: Please enter 2+ characters");
-    return "Please enter 2 or more characters";
+  } else if (input.value.length < min) {
+    return errorMessage({minCharLength: min - 1}).minCharError;
   } else {
     return null;
   }
 }
 
 function emailCheck(input) {
-  console.log("emailCheck called with input:", input)
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   if(input.value.trim() === "" || !emailRegex.test(input.value)) {
-    return "Please enter a valid email address";
+    return errorMessage().emailError;
   } else {
     return null;
   }
@@ -189,13 +219,13 @@ function ageCheck(input) {
   console.log("ageCheck called with input:", input)
   const ageRegex = /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/;
   if (input.value.trim() === "" || !ageRegex.test(input.value)) {
-    return "Please enter your date of birth"
+    return errorMessage().birthdateError;
   } else {
     return null;
   }
 }
 
-function checkQuantity(input) {
+function quantityCheck(input) {
   if (isNaN(input.value) || input.value < 1 || input.value.trim() === "") {
     return "Please enter the amount of tournaments";
   } else {
@@ -203,98 +233,57 @@ function checkQuantity(input) {
   }
 }
 
-// //Custom validation checks
-// function handleNameValidation(inputParent) {
-//   const input = inputParent.querySelector("input");
-//   let inputError;
+function locationCheck(input) {
+  const radioButtons = input.getElementsByName("location");
+  let radioButtonChecked = false;
 
-//   if (input.value.trim() === "") {
-//     if (input.name === "first") {
-//       inputError = "Please enter your first name";
-//     } else {
-//       inputError = "Please enter your last name";
-//     }
-//   } else if (input.value.length < 3) {
-//     inputError = "Please enter 2 or more characters";
-//   }
+  for (let i = 0; i < radioButtons.length; i++) {
+    if (radioButtons[i].checked) {
+      radioButtonChecked = true;
+      break;
+    }
+  }
 
-//   addInvalidityStyles(inputParent, input, inputError);
-// }
-
-// function handleEmailValidation(inputParent) {
-//   const input = inputParent.querySelector("input");
-//   let inputError;
-
-//   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-//   if(input.value.trim() === "" || !emailRegex.test(input.value)) {
-//     inputError = "Please enter a valid email address";
-//   }
-
-//   addInvalidityStyles(inputParent, input, inputError);
-// }
-
-
-// function handleAgeValidation(inputParent) {
-//   const input = inputParent.querySelector("input");
-//   let inputError;
-
-//   const ageRegex = /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/;
-//   if (input.value.trim() === "" || !ageRegex.test(input.value)) {
-//     inputError = "Please enter your date of birth"
-//   }
-
-//   addInvalidityStyles(inputParent, input, inputError);
-// }
-
-// function handleQuantityValidation(inputParent) {
-//   const input = inputParent.querySelector("input");
-//   let inputError;
-
-//   if (isNaN(input.value) || input.value < 1 || input.value.trim() === "") {
-//     inputError = "Please enter the amount of tournaments";
-//   }
-
-//   addInvalidityStyles(inputParent, input, inputError);
-// }
-
-// function handleConditionsValidation(inputParent) {
-//   const input = inputParent.querySelector("input");
-//   let inputError = inputParent.dataset.error;
-
-// }
+  if (!radioButtonChecked) {
+    return "Please choose a location"
+  } else {
+    return null
+  }
+}
 
 //Check inputs of each formData to see if they are valid according to customValidation
 //Today's date used for max value of date input
 formData.forEach(data => {
   const input = data.querySelector("input");
   if (input.name === "birthdate") {
-    input.setAttribute("max", getTodaysdate())
+    input.setAttribute("max", getTodaysDate())
   }
 
   input.addEventListener("change", () => handleValidation(data));
-
-  // switch (input.type) {
-  //   case "text": 
-  //   input.addEventListener("change", () => handleNameValidation(data));
-  //   break;
-  // case "email":
-  //   input.addEventListener("change", () => handleEmailValidation(data));
-  //   break;
-  // case "date": 
-  //   input.addEventListener("change", () => handleAgeValidation(data));
-  //   break;
-  // case "number":
-  //   input.addEventListener("change", () => handleQuantityValidation(data));
-  // }
 });
 
 //Process form information
 function processFormSubmission(e) {
   e.preventDefault();
-  getUserData();
-  form.reset();
-  closeModal(modalbg);
-  launchModal(modalConf)
+
+  let formIsValid = true;
+
+  formData.forEach(data => {
+    const input = data.querySelector("input");
+    if (!input.value || input.value.trim() === "") {
+      input.setCustomValidity("Did you see this field?");
+      formIsValid = false;
+    } else {
+      input.setCustomValidity("");
+    }
+  });
+
+  if (formIsValid) {
+    getUserData();
+    form.reset();
+    closeModal(modalbg);
+    launchModal(modalConf);
+  }
 }
 
 //Submit form
